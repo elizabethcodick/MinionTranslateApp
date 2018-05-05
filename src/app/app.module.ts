1c441433cd6fcc6687f22bbbb2d174fd7eb280e3
Elizabeth Codick <elizabethcodick@Elizabeths-Air.wireless.rit.edu>
2018-05-04 22:36:04 -0400

    minion2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-//import { Http, Headers, RequestOptions } from '@angular/http';
 import { HttpModule } from '@angular/http';
 import { Media } from '@ionic-native/media';
 
